57ddafaff81e30ef2f49324103e97baf1d5e6538
Arthur Decker <molayodecker@gmail.com>
2026-09-21 09:23:54 -0400

    config: make Sumsub KYC level generic

--- config/runtime.exs
+++ config/runtime.exs
@@ -146,8 +146,9 @@ if sumsub_webhook_secret = System.get_env("SUMSUB_WEBHOOK_SECRET") do
   config :mithril, :sumsub_webhook_secret, sumsub_webhook_secret
 end
 
-if sumsub_worker_level_name = System.get_env("SUMSUB_WORKER_LEVEL_NAME") do
-  config :mithril, :sumsub_worker_level_name, sumsub_worker_level_name
+if sumsub_level_name =
+     System.get_env("SUMSUB_LEVEL_NAME") || System.get_env("SUMSUB_WORKER_LEVEL_NAME") do
+  config :mithril, :sumsub_level_name, sumsub_level_name
 end
 
 if tax_subaccount = System.get_env("PAYSTACK_TAX_SUBACCOUNT") do
