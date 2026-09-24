4819288197fef4c6d2bb14ad5040686df6ce39e4
Arthur Decker <molayodecker@gmail.com>
2026-09-24 06:31:03 -0400

    style: format RPC numeric normalization

--- lib/mithril/mobile_gateway.ex
+++ lib/mithril/mobile_gateway.ex
@@ -302,7 +302,8 @@ defmodule Mithril.MobileGateway do
 
   defp normalize_rpc_value("numeric", %Decimal{} = value), do: {:ok, value}
   defp normalize_rpc_value("numeric", value) when is_integer(value), do: {:ok, Decimal.new(value)}
-  defp normalize_rpc_value("numeric", value) when is_float(value), do: {:ok, Decimal.from_float(value)}
+  defp normalize_rpc_value("numeric", value) when is_float(value),
+    do: {:ok, Decimal.from_float(value)}
 
   defp normalize_rpc_value("numeric", value) when is_binary(value) do
     case Decimal.parse(value) do
